806861878a29e378845055d9895cc003ad6886e2
Rehan Abbas <abbasrehan054@gmail.com>
2024-06-26 18:15:01 +0530

    Fixes the mongoose error

--- connection.js
+++ connection.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const MONGO_URL = process.env.MONGO_URI
+mongoose.set('strictQuery',true);
 mongoose.connect(MONGO_URL, ()=> {
   console.log('connected to mongodb')
 })
